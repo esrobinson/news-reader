b03c5624dc073cf67ce1b220b0432846da5fe1a0
App Academy Student <student@appacademy.io>
2014-01-09 14:03:26 -0800

    Add feed show page

--- app/assets/javascripts/models/feed.js
+++ app/assets/javascripts/models/feed.js
@@ -2,7 +2,6 @@ NewsReader.Models.Feed = Backbone.Model.extend({
 
 	parse: function(data){
 		var entries = data.entries;
-		console.log(this)
 		data.entries = new NewsReader.Collections.Entries(entries, { feed: this });
 
 		return data;
---
+++ app/assets/javascripts/views/feed.js
@@ -0,0 +1,15 @@
+NewsReader.Views.Feed = Backbone.View.extend({
+
+	template: JST["feeds/show"],
+
+	render: function () {
+		var content = this.template({ feed: this.model });
+		this.$el.html(content);
+		return this;
+	},
+
+	refresh: function () {
+
+	}
+
+})
